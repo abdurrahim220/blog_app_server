bc59ea820042c77ba818169bb7f1059a133f7503
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-06 23:41:17 +0600

    models are added

---
+++ models/Comments.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const CommentsSchema = new mongoose.Schema(
+    {
+        comment:{
+            type:String,
+            require:true,
+        },
+        author:{
+            type:String,
+            require:true,
+        },
+        postId:{
+            type:String,
+            require:true,
+        },
+        userId:{
+            type:String,
+            require:true,
+        },
+    },{timestamps:true}
+)
+
+module.exports = mongoose.model("Comments",CommentsSchema)
