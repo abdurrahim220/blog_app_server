774e552fd4c5b3f6c3bc5b91ede2c00e14755a3d
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-06 23:29:21 +0600

    database is connected

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
+
 
 //database
-
 const connectDB = async () => {
   try {
-    await mongoose.connect();
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("data base is connected is successfully")
   } catch (error) {
     console.log(error);
   }
 };
 
-app.listen(5000, () => {
-  console.log("app is running on port");
+// middlewares
+
+
+
+// server running
+app.listen(process.env.PORT, () => {
+    connectDB()
+  console.log("app is running on port ",process.env.PORT);
 });
