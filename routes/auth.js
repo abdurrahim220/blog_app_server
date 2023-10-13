6e4a8cf9d19f32caf4f272d65dbe27085ac40868
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-14 00:18:45 +0600

    database is connected and location setup done

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,14 @@
 const express = require('express')
-
 const router = express.Router()
-
 const User = require('../models/User');
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 // const jwt = require('jsonwebtoken')
 
 // register
 
 router.post('/register',async(req,res)=>{
     try {
+        req.body.password = await bcrypt.hash(req.body.password,11)
         const {username,email,password} =req.body;
         const newUser = new User({username,email,password})
         const saveUser = await newUser.save()
